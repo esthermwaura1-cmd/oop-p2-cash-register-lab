3b8f4be4e8bb582a4e06fc1c0c484dbefbc49e8a
Esther Mwaura <esther.mwaura1@student.moringaschool.com>
2026-06-15 12:46:03 -0700

    final cash register fix

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,25 +1,47 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-    def __init__(self, discount=0):
-        self.discount = discount
-        self.total = 0
-        self.items = []
+  def __init__(self,discount=0):
+    self._discount = discount 
+    self.total = 0
+    self.items = []
+    self.previous_transactions =[]
 
-    def add_item(self, title, price, quantity=1):
-        for _ in range(quantity):
-            self.items.append(title)
-        self.total += price * quantity
 
-    def apply_discount(self):
-        if self.discount == 0:
-            print("There is no discount to apply.")
-            return
+  @property
+  def discount(self):
+    return self._discount 
+  
+  @discount.setter
+  def discount (self,value):
+    if isinstance(value,int) and 0 < value <= 100:
+      self._discount =value
+    else:  
+      print('Not valid discount')
 
-        self.total = self.total - (self.total * self.discount / 100)
+  
+    
 
-        print(f"After the discount, the total comes to ${self.total}.")
 
-    def void_last_transaction(self):
-        if self.items:
-            self.items.pop()
+  def add_item(self,item,price,quantity=1):
+    self.total += price * quantity
+
+    self.items.append(item)
+
+    self.previous_transactions.append(price*quantity)
+
+
+
+  def apply_discount(self):
+    if self._discount > 0:
+       self.total -= self.total * self._discount /100
+       print(f'After the discount, the total comes to ${self.total:.2f}.')
+    else:  
+       print('There is no discount to apply.')
+
+  def void_last_transaction(self):
+    self.items.pop()
+    self.total -= self.previous_transactions.pop()
+       
+
+
